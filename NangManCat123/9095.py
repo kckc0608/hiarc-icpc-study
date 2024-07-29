69f567c8b98f479c9f9a03765e2c360eb1ee8050
NangManCat123 <nangmancat123@gmail.com>
2024-07-29 13:11:03 +0900

    solve 9095

---
+++ NangManCat123/9095.py
@@ -0,0 +1,14 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    dp=[0]*(12)
+    for i in range(1,n+1):
+        if i==1:
+            dp[i]=1
+        elif i==2:
+            dp[i]=2
+        elif i==3:
+            dp[i]=4
+        else:
+            dp[i]=dp[i-3]+dp[i-2]+dp[i-1]
+    print(dp[n])
